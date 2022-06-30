34ae5de8264ef545193a0290562fb80222c9ec78
ali85k <aliceanndraad56@gmail.com>
2022-06-30 19:22:51 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,23 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+   }
+  
+  
+  let contadorD = 10
+  
+   while(contadorD >= 1){
+      console.log(contadorD--)
+  }
+  
+  
+    for(let impar = 1; impar <=  100; impar+=2){
+      console.log(impar)
+    }
+  
+  
+  let number = 0
+  
+  while(number <= 100){
+     console.log(number)
+     number+=2
+   }
